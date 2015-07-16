7dc9983b6bffd891d1668ff09e581557565a77d8
Eric Aldrich <accordltn@gmail.com>
2015-07-16 08:26:57 -0700

    bubble_sort_by finished

--- ruby/project-advanced-building-blocks/bubblesort.rb
+++ ruby/project-advanced-building-blocks/bubblesort.rb
@@ -14,5 +14,23 @@ def bubble_sort (list_array)
   list_array.to_s
 end
 
-puts [4,3,78,2,0,2].to_s
 puts bubble_sort([4,3,78,2,0,2])
+
+def bubble_sort_by (list_array)
+  loop do
+    swap = 0
+    list_array.each_with_index do |x, index|
+      if list_array[index + 1] == nil
+      elsif yield(list_array[index],list_array[index + 1]) < 0
+        swap = list_array[index]
+        list_array[index] = list_array[index + 1]
+        list_array[index + 1] = swap
+      end
+    end
+    break if swap == 0
+  end
+  list_array.to_s
+end
+
+length_sorting = Proc.new {|left,right| right.length - left.length}
+puts bubble_sort_by(["hi","hello","hey"], &length_sorting)
