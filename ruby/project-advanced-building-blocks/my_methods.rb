f037da775c1b9817392a7360b60075a11cd68f84
Eric Aldrich <accordltn@gmail.com>
2015-07-17 04:11:02 -0700

    my_each isn't working just yet, not sure why the proc isn't properly changing the variable its yielded.

---
+++ ruby/project-advanced-building-blocks/my_methods.rb
@@ -0,0 +1,22 @@
+#module Enumerable
+  def my_each(my_array)
+    my_array.split('') if my_array.is_a? String
+    i = 0
+    while i < my_array.length
+      yield (my_array[i])
+      i += 1
+      puts "#{my_array[i]}"
+    end
+    my_array.to_s
+  end
+#end
+
+add_one = Proc.new {|x| 
+  x += 1
+  puts "#{x}"
+}
+puts my_each([1, 1, 1], &add_one)
+
+
+
+
