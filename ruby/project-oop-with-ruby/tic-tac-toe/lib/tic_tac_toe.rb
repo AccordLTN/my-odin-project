4c86d8f895b99e756223b49f9724cc4ad0c63d0a
Eric Aldrich <accordltn@gmail.com>
2015-07-20 22:29:26 -0700

    Board class and tests half implemented.

--- ruby/project-oop-with-ruby/tic-tac-toe/lib/tic_tac_toe.rb
+++ ruby/project-oop-with-ruby/tic-tac-toe/lib/tic_tac_toe.rb
@@ -3,6 +3,7 @@
 require_relative "./tic_tac_toe/version.rb"
 require_relative "./tic_tac_toe/cell.rb"
 require_relative "./tic_tac_toe/player.rb"
+require_relative "./tic_tac_toe/board.rb"
 
 module TicTacToe
 end
