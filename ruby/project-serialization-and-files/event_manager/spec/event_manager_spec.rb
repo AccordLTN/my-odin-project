345ea095a2f072654290967f7a5cb8b4f6bfc48b
Eric Aldrich <accordltn@gmail.com>
2015-07-22 17:33:57 -0700

    Initial commit of EventManager module.

---
+++ ruby/project-serialization-and-files/event_manager/spec/event_manager_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+describe EventManager do
+  it 'has a version number' do
+    expect(EventManager::VERSION).not_to be nil
+  end
+
+  it 'does something useful' do
+    expect(false).to eq(true)
+  end
+end
