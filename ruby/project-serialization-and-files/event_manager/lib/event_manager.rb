ce3c322e38e22c6d84275a9d0e29436b11464cd0
Eric Aldrich <accordltn@gmail.com>
2015-07-23 20:45:32 -0700

    Base assignment completed.

--- ruby/project-serialization-and-files/event_manager/lib/event_manager.rb
+++ ruby/project-serialization-and-files/event_manager/lib/event_manager.rb
@@ -1,33 +1,44 @@
 require_relative "./event_manager/version"
 
 require "csv"
+require "erb"
+require "sunlight/congress"
+
+Sunlight::Congress.api_key = "e179a6973728c4dd3fb1204283aaccb5"
 
 def clean_zipcode(zipcode)
   zipcode.to_s.rjust(5,"0")[0..4]
 end
 
+def legislators_by_zipcode(zipcode)
+  Sunlight::Congress::Legislator.by_zipcode(zipcode)
+end
+
+def save_thank_you_letters(id,form_letter)
+  Dir.mkdir("output") unless Dir.exists?("output")
+
+  filename = "output/thanks_#{id}.html"
+
+  File.open(filename,'w') do |file|
+    file.puts form_letter
+  end
+end
+
 puts "EventManager initialized."
 
 contents = CSV.open "./data/event_attendees.csv", headers: true, header_converters: :symbol
+
+template_letter = File.read "./form/form_letter.erb"
+erb_template = ERB.new template_letter
+
 contents.each do |row|
-  name = row[:first_name]
+  id = row[0]
+  name = row[:first_name].capitalize
   zipcode = clean_zipcode(row[:zipcode])
-  puts "#{name} #{zipcode}"
+  legislators = legislators_by_zipcode(zipcode)
+
+  form_letter = erb_template.result(binding)
+  save_thank_you_letters(id,form_letter)
+
+  puts "#{id} #{name} #{zipcode}"
 end
-
-
-# lines = (File.read "./data/event_attendees.csv").split("\n")
-# lines.each_with_index do |line, index|
-#   next if index == 0
-#   columns = line.split(",")
-#   name = columns[2].capitalize
-#   puts name
-# end
-
-
-# def clean_zipcode(zipcode)
-#   return zipcode = "00000" if zipcode.nil?
-#   return zipcode.slice(0..4) if zipcode.length > 5
-#   zipcode.insert(0,"0") while zipcode.length < 5
-#   zipcode
-# end
